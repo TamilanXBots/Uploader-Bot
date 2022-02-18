5913a0b004d00c1f841182fc3f370ea6e2f2a6f0
TamilanXBots <99972200+TamilanXBots@users.noreply.github.com>
2022-02-18 19:19:02 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -221,11 +221,11 @@ async def echo(bot, update):
                 "video", format_id, format_ext, randem)
             inline_keyboard.append([
                 InlineKeyboardButton(
-                    "Video",
+                    "SVideo",
                     callback_data=(cb_string_video).encode("UTF-8")
                 ),
                 InlineKeyboardButton(
-                    "File",
+                    "DFile",
                     callback_data=(cb_string_file).encode("UTF-8")
                 )
             ])
